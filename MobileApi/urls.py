be5c4842488af28ea6e9360c3be4f196573211f0
iamdamified <119352851+iamdamified@users.noreply.github.com>
2023-05-14 16:40:08 +0100

    initial

---
+++ MobileApi/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from Web.views import 
+
+
+# urlpatterns = [
+#     path('/', , name= ),
+#     path('/', , name= ),
+#     path('/', , name= )
+
+# ]
---
+++ Web/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# from Web.views import 
+
+
+# urlpatterns = [
+#     path('/', , name= ),
+#     path('/', , name= ),
+#     path('/', , name= )
+
+# ]
